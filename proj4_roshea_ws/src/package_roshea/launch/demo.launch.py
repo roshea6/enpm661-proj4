61a3a4ea501cd30d32a60c7a2a589dac0a3c8b54
Ryan O'Shea <rpo1202@gmail.com>
2024-04-20 01:29:21 -0400

    Reformatted arm package according to online tutorial

--- proj4_roshea_ws/src/package_roshea/launch/demo.launch.py
+++ proj4_roshea_ws/src/package_roshea/launch/demo.launch.py
@@ -3,5 +3,15 @@ from moveit_configs_utils.launches import generate_demo_launch
 
 
 def generate_launch_description():
+    # Static TF
+    # static_tf = Node(
+    #     package="tf2_ros",
+    #     executable="static_transform_publisher",
+    #     name="static_transform_publisher",
+    #     output="log",
+    #     arguments=["0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "world_frame", "panda_link0"],
+    # )
+    
+    
     moveit_config = MoveItConfigsBuilder("panda", package_name="package_roshea").to_moveit_configs()
     return generate_demo_launch(moveit_config)
